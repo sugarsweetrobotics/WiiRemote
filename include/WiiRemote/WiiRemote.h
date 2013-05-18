9e35ccd7ee6398fe77b97bd33aeb3226d0ee80cc
Yuki Suga <ysuga@ysuga.net>
2013-05-18 23:02:51 +0900

    This if first commit. This repository is generated by wasanbon

---
+++ include/WiiRemote/WiiRemote.h
@@ -0,0 +1,340 @@
+// -*- C++ -*-
+/*!
+ * @file  WiiRemote.h
+ * @brief Wii Remote RTC
+ * @date  $Date$
+ *
+ * $Id$
+ */
+
+#ifndef WIIREMOTE_H
+#define WIIREMOTE_H
+
+#include <rtm/Manager.h>
+#include <rtm/DataFlowComponentBase.h>
+#include <rtm/CorbaPort.h>
+#include <rtm/DataInPort.h>
+#include <rtm/DataOutPort.h>
+#include <rtm/idl/BasicDataTypeSkel.h>
+#include <rtm/idl/ExtendedDataTypesSkel.h>
+#include <rtm/idl/InterfaceDataTypesSkel.h>
+
+// Service implementation headers
+// <rtc-template block="service_impl_h">
+
+// </rtc-template>
+
+#include "wiicpp.h"
+
+// Service Consumer stub headers
+// <rtc-template block="consumer_stub_h">
+
+// </rtc-template>
+
+using namespace RTC;
+
+/*!
+ * @class WiiRemote
+ * @brief Wii Remote RTC
+ *
+ */
+class WiiRemote
+  : public RTC::DataFlowComponentBase
+{
+ public:
+  /*!
+   * @brief constructor
+   * @param manager Maneger Object
+   */
+  WiiRemote(RTC::Manager* manager);
+
+  /*!
+   * @brief destructor
+   */
+  ~WiiRemote();
+
+  // <rtc-template block="public_attribute">
+  
+  // </rtc-template>
+
+  // <rtc-template block="public_operation">
+  
+  // </rtc-template>
+
+  /***
+   *
+   * The initialize action (on CREATED->ALIVE transition)
+   * formaer rtc_init_entry() 
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+   virtual RTC::ReturnCode_t onInitialize();
+
+  /***
+   *
+   * The finalize action (on ALIVE->END transition)
+   * formaer rtc_exiting_entry()
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onFinalize();
+
+  /***
+   *
+   * The startup action when ExecutionContext startup
+   * former rtc_starting_entry()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onStartup(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The shutdown action when ExecutionContext stop
+   * former rtc_stopping_entry()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onShutdown(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The activated action (Active state entry action)
+   * former rtc_active_entry()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+   virtual RTC::ReturnCode_t onActivated(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The deactivated action (Active state exit action)
+   * former rtc_active_exit()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+   virtual RTC::ReturnCode_t onDeactivated(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The execution action that is invoked periodically
+   * former rtc_active_do()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+   virtual RTC::ReturnCode_t onExecute(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The aborting action when main logic error occurred.
+   * former rtc_aborting_entry()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onAborting(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The error action in ERROR state
+   * former rtc_error_do()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onError(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The reset action that is invoked resetting
+   * This is same but different the former rtc_init_entry()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onReset(RTC::UniqueId ec_id);
+  
+  /***
+   *
+   * The state update action that is invoked after onExecute() action
+   * no corresponding operation exists in OpenRTm-aist-0.2.0
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onStateUpdate(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The action that is invoked when execution context's rate is changed
+   * no corresponding operation exists in OpenRTm-aist-0.2.0
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onRateChanged(RTC::UniqueId ec_id);
+
+
+ protected:
+  // <rtc-template block="protected_attribute">
+  
+  // </rtc-template>
+
+  // <rtc-template block="protected_operation">
+  
+  // </rtc-template>
+
+  // Configuration variable declaration
+  // <rtc-template block="config_declare">
+  /*!
+   * 
+   * - Name:  is_bar_above
+   * - DefaultValue: 1
+   */
+  int m_is_bar_above;
+  /*!
+   * 
+   * - Name:  aspect_ratio
+   * - DefaultValue: 4_3
+   */
+  std::string m_aspect_ratio;
+  /*!
+   * 
+   * - Name:  virtual_screen_width
+   * - DefaultValue: 640
+   */
+  int m_virtual_screen_width;
+  /*!
+   * 
+   * - Name:  virtual_screen_height
+   * - DefaultValue: 480
+   */
+  int m_virtual_screen_height;
+
+  // </rtc-template>
+
+  // DataInPort declaration
+  // <rtc-template block="inport_declare">
+  RTC::TimedBooleanSeq m_leds;
+  /*!
+   */
+  InPort<RTC::TimedBooleanSeq> m_ledsIn;
+  RTC::TimedBoolean m_rumble;
+  /*!
+   */
+  InPort<RTC::TimedBoolean> m_rumbleIn;
+  
+  // </rtc-template>
+
+
+  // DataOutPort declaration
+  // <rtc-template block="outport_declare">
+  RTC::TimedBooleanSeq m_buttons;
+  /*!
+   * Up, Down, Right, Left, One, Two, A, B, Plus, Minus, Home
+   */
+  OutPort<RTC::TimedBooleanSeq> m_buttonsOut;
+  RTC::TimedAngularAcceleration3D m_accel;
+  /*!
+   */
+  OutPort<RTC::TimedAngularAcceleration3D> m_accelOut;
+  RTC::TimedOrientation3D m_orientation;
+  /*!
+   */
+  OutPort<RTC::TimedOrientation3D> m_orientationOut;
+  RTC::TimedPoint2D m_cursor;
+  /*!
+   */
+  OutPort<RTC::TimedPoint2D> m_cursorOut;
+  RTC::TimedLong m_distance;
+  /*!
+   */
+  OutPort<RTC::TimedLong> m_distanceOut;
+  RTC::TimedPoint2D m_ir;
+  /*!
+   */
+  OutPort<RTC::TimedPoint2D> m_irOut;
+  
+  // </rtc-template>
+
+  // CORBA Port declaration
+  // <rtc-template block="corbaport_declare">
+  
+  // </rtc-template>
+
+  // Service declaration
+  // <rtc-template block="service_declare">
+  
+  // </rtc-template>
+
+  // Consumer declaration
+  // <rtc-template block="consumer_declare">
+  
+  // </rtc-template>
+
+ private:
+  // <rtc-template block="private_attribute">
+  
+  // </rtc-template>
+
+  // <rtc-template block="private_operation">
+  
+  // </rtc-template>
+
+  CWii m_Wii;
+
+  int m_numFound;
+
+  int m_is_bar_above_old;
+  std::string m_aspect_ratio_old;
+  int m_virtual_screen_width_old;
+  int m_virtual_screen_height_old;
+
+};
+
+
+extern "C"
+{
+  DLL_EXPORT void WiiRemoteInit(RTC::Manager* manager);
+};
+
+#endif // WIIREMOTE_H
---
+++ src/WiiRemote.cpp
@@ -0,0 +1,388 @@
+// -*- C++ -*-
+/*!
+ * @file  WiiRemote.cpp
+ * @brief Wii Remote RTC
+ * @date $Date$
+ *
+ * $Id$
+ */
+
+#include "WiiRemote.h"
+
+// Module specification
+// <rtc-template block="module_spec">
+static const char* wiiremote_spec[] =
+  {
+    "implementation_id", "WiiRemote",
+    "type_name",         "WiiRemote",
+    "description",       "Wii Remote RTC",
+    "version",           "1.0.0",
+    "vendor",            "Sugar Sweet Robotics",
+    "category",          "Input",
+    "activity_type",     "PERIODIC",
+    "kind",              "DataFlowComponent",
+    "max_instance",      "1",
+    "language",          "C++",
+    "lang_type",         "compile",
+    // Configuration variables
+    "conf.default.is_bar_above", "1",
+    "conf.default.aspect_ratio", "4_3",
+    "conf.default.virtual_screen_width", "640",
+    "conf.default.virtual_screen_height", "480",
+    // Widget
+    "conf.__widget__.is_bar_above", "text",
+    "conf.__widget__.aspect_ratio", "ordered_list",
+    "conf.__widget__.virtual_screen_width", "ordered_list",
+    "conf.__widget__.virtual_screen_height", "ordered_list",
+    // Constraints
+    "conf.__constraints__.aspect_ratio", "4_3, 16_9",
+    ""
+  };
+// </rtc-template>
+
+/*!
+ * @brief constructor
+ * @param manager Maneger Object
+ */
+WiiRemote::WiiRemote(RTC::Manager* manager)
+    // <rtc-template block="initializer">
+  : RTC::DataFlowComponentBase(manager),
+    m_ledsIn("leds", m_leds),
+    m_rumbleIn("rumble", m_rumble),
+    m_buttonsOut("buttons", m_buttons),
+    m_accelOut("accel", m_accel),
+    m_orientationOut("orientation", m_orientation),
+    m_cursorOut("cursor", m_cursor),
+    m_distanceOut("distance", m_distance),
+    m_irOut("ir", m_ir)
+
+    // </rtc-template>
+
+{
+  m_numFound = 0;
+}
+
+/*!
+ * @brief destructor
+ */
+WiiRemote::~WiiRemote()
+{
+}
+
+
+
+RTC::ReturnCode_t WiiRemote::onInitialize()
+{
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
+  addInPort("leds", m_ledsIn);
+  addInPort("rumble", m_rumbleIn);
+  
+  // Set OutPort buffer
+  addOutPort("buttons", m_buttonsOut);
+  addOutPort("accel", m_accelOut);
+  addOutPort("orientation", m_orientationOut);
+  addOutPort("cursor", m_cursorOut);
+  addOutPort("distance", m_distanceOut);
+  addOutPort("ir", m_irOut);
+  
+  // Set service provider to Ports
+  
+  // Set service consumers to Ports
+  
+  // Set CORBA Service Ports
+  
+  // </rtc-template>
+
+  // <rtc-template block="bind_config">
+  // Bind variables and configuration variable
+  bindParameter("is_bar_above", m_is_bar_above, "1");
+  bindParameter("aspect_ratio", m_aspect_ratio, "4_3");
+  bindParameter("virtual_screen_width", m_virtual_screen_width, "640");
+  bindParameter("virtual_screen_height", m_virtual_screen_height, "480");
+  // </rtc-template>
+  
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t WiiRemote::onFinalize()
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t WiiRemote::onStartup(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t WiiRemote::onShutdown(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+RTC::ReturnCode_t WiiRemote::onActivated(RTC::UniqueId ec_id)
+{
+  if (m_numFound == 0) {
+    std::cout << "Please turn on your WiiRemote." << std::endl;
+    m_numFound = m_Wii.Find(1);
+    if (m_numFound != 1) {
+      std::cout << "Can not find Wii Remote." << std::endl;
+      return RTC_ERROR;
+    }
+
+    m_Wii.Connect();
+  } else {
+    // Reload Wiimotes.
+    m_Wii.GetWiimotes();
+  }
+
+  m_buttons.data.length(11);
+  m_is_bar_above_old = !m_is_bar_above;
+  if (m_aspect_ratio ==  "4_3") {
+    m_aspect_ratio_old = "16_9";
+  } else {
+    m_aspect_ratio_old = "4_3";
+  }
+  m_virtual_screen_width = 0;
+  m_virtual_screen_height = 0;
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t WiiRemote::onDeactivated(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t WiiRemote::onExecute(RTC::UniqueId ec_id)
+{
+  std::vector<CWiimote>& m_Wiimotes = m_Wii.GetWiimotes(0);
+  CWiimote& wiimote = m_Wiimotes[0];
+
+  if (m_is_bar_above != m_is_bar_above_old) {
+    if (m_is_bar_above) {
+      wiimote.IR.SetBarPosition(CIR::BAR_ABOVE);
+    } else {
+      wiimote.IR.SetBarPosition(CIR::BAR_BELOW);
+    }
+  }
+  m_is_bar_above_old = m_is_bar_above;
+
+  if (m_aspect_ratio != m_aspect_ratio_old) {
+    if (m_aspect_ratio == "4_3") {
+      wiimote.IR.SetAspectRatio(CIR::ASPECT_4_3);
+    } else if (m_aspect_ratio == "16_9") {
+      wiimote.IR.SetAspectRatio(CIR::ASPECT_16_9);
+    } else {
+      std::cout << "WiiRemote:: unknow aspect ratio." << std::endl;
+      return RTC_ERROR;
+    }
+  }
+  m_aspect_ratio_old = m_aspect_ratio;
+
+  if ((m_virtual_screen_width != m_virtual_screen_width_old) || 
+      (m_virtual_screen_height != m_virtual_screen_height_old)) {
+    wiimote.IR.SetVres(m_virtual_screen_width, m_virtual_screen_height);
+  }
+  m_virtual_screen_width_old = m_virtual_screen_width;
+  m_virtual_screen_height_old = m_virtual_screen_height;
+
+  if (m_ledsIn.isNew()) {
+    m_ledsIn.read();
+    int led = 0;
+    int flag[4] = {CWiimote::LED_1, CWiimote::LED_2, CWiimote::LED_3, CWiimote::LED_4};
+    for (unsigned int i = 0;i < 4 || i < m_leds.data.length();i++) {
+      led |= flag[i];
+    }
+    wiimote.SetLEDs(led);
+  }
+
+  if (m_rumbleIn.isNew()) {
+    m_rumbleIn.read();
+    if (m_rumble.data) {
+      wiimote.SetRumbleMode(CWiimote::ON);
+    } else {
+      wiimote.SetRumbleMode(CWiimote::OFF);
+    }
+  }
+
+  const int buttons[] = {
+    CButtons::BUTTON_UP, CButtons::BUTTON_DOWN, CButtons::BUTTON_RIGHT, CButtons::BUTTON_LEFT,
+    CButtons::BUTTON_ONE, CButtons::BUTTON_TWO, CButtons::BUTTON_A, CButtons::BUTTON_B,
+    CButtons::BUTTON_PLUS, CButtons::BUTTON_MINUS, CButtons::BUTTON_HOME };
+
+  float r, p, y;
+  int i;
+  if (m_Wii.Poll()) {
+    switch(wiimote.GetEvent()) {
+    case CWiimote::EVENT_EVENT:
+      for (i = 0;i < 11;i++) {
+	if (wiimote.Buttons.isPressed(buttons[i])) {
+	  m_buttons.data[i] = true;
+	} else {
+	  m_buttons.data[i] = false;
+	}
+      }
+      m_buttonsOut.write();
+      
+      if (!wiimote.isUsingACC()) {
+	wiimote.SetMotionSensingMode(CWiimote::ON);
+      }
+
+      wiimote.Accelerometer.GetOrientation(p, r, y);
+      m_orientation.data.p = p; m_orientation.data.y = y; m_orientation.data.r = r;
+      m_orientationOut.write();
+      
+      wiimote.Accelerometer.GetRawOrientation(p, r);
+      m_accel.data.aax = r; m_accel.data.aay = p; m_accel.data.aaz = 0;
+      m_accelOut.write();
+
+      if (!wiimote.isUsingIR()) {
+	wiimote.IR.SetMode(CIR::ON);
+      }
+      
+      if (wiimote.isUsingIR()) {
+	std::vector<CIRDot>& dots = wiimote.IR.GetDots();
+	std::vector<CIRDot>::iterator it;
+	int x, y;
+	for (it = dots.begin(); it != dots.end(); ++it) {
+	  if ((*it).isVisible()) {
+	    (*it).GetCoordinate(x, y);
+	    m_ir.data.x = x; m_ir.data.y = y;
+	    m_irOut.write();
+	    
+	    wiimote.IR.GetCursorPosition(x, y);
+	    m_cursor.data.x = x; m_cursor.data.y = y;
+	    m_cursorOut.write();
+
+	    m_distance.data = wiimote.IR.GetDistance();
+	    m_distanceOut.write();
+	  }
+	}
+      }
+
+      break;
+      
+    case CWiimote::EVENT_STATUS:
+      //HandleStatus(wiimote);
+      break;
+      
+    case CWiimote::EVENT_DISCONNECT:
+    case CWiimote::EVENT_UNEXPECTED_DISCONNECT:
+      //HandleDisconnect(wiimote);
+      //reloadWiimotes = 1;
+      m_Wiimotes = m_Wii.GetWiimotes();
+      break;
+      
+    case CWiimote::EVENT_READ_DATA:
+      //HandleReadData(wiimote);
+      break;
+      
+    case CWiimote::EVENT_NUNCHUK_INSERTED:
+      //HandleNunchukInserted(wiimote);
+      //reloadWiimotes = 1;
+      m_Wiimotes = m_Wii.GetWiimotes();
+      break;
+      
+    case CWiimote::EVENT_CLASSIC_CTRL_INSERTED:
+      //HandleClassicInserted(wiimote);
+      //reloadWiimotes = 1;
+      m_Wiimotes = m_Wii.GetWiimotes();
+      break;
+      
+    case CWiimote::EVENT_GUITAR_HERO_3_CTRL_INSERTED:
+      //HandleGH3Inserted(wiimote);
+      //reloadWiimotes = 1;
+      m_Wiimotes = m_Wii.GetWiimotes();
+      break;
+      
+    case CWiimote::EVENT_MOTION_PLUS_INSERTED:
+      cout << "Motion Plus inserted." << endl;
+      break;
+      
+    case CWiimote::EVENT_BALANCE_BOARD_INSERTED:
+      cout << "Balance Board connected.\n"  << endl;
+      break;
+      
+    case CWiimote::EVENT_BALANCE_BOARD_REMOVED:
+      cout << "Balance Board disconnected.\n"  << endl;
+      break;
+      
+    case CWiimote::EVENT_NUNCHUK_REMOVED:
+    case CWiimote::EVENT_CLASSIC_CTRL_REMOVED:
+    case CWiimote::EVENT_GUITAR_HERO_3_CTRL_REMOVED:
+    case CWiimote::EVENT_MOTION_PLUS_REMOVED:
+      cout << "An expansion was removed." << endl;
+      //HandleStatus(wiimote);
+      //reloadWiimotes = 1;
+      m_Wiimotes = m_Wii.GetWiimotes();
+      break;
+      
+    default:
+      break;
+    } // switch
+  }
+  
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t WiiRemote::onAborting(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t WiiRemote::onError(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t WiiRemote::onReset(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t WiiRemote::onStateUpdate(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t WiiRemote::onRateChanged(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+
+extern "C"
+{
+ 
+  void WiiRemoteInit(RTC::Manager* manager)
+  {
+    coil::Properties profile(wiiremote_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<WiiRemote>,
+                             RTC::Delete<WiiRemote>);
+  }
+  
+};
+
+
